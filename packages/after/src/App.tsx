9002f7df159a2bc09c939c00050091c68f6d8743
윤지 <85012406+yoonhihi97@users.noreply.github.com>
2025-11-22 23:14:48 +0900

    chore: index.css import 추가

--- packages/after/src/App.tsx
+++ packages/after/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Header } from './components/organisms'
-import { ManagementPage } from './pages/ManagementPage'
-import './styles/components.css'
+import React from 'react';
+import { Header } from './components/organisms';
+import { ManagementPage } from './pages/ManagementPage';
+import './styles/components.css';
 
 export const App: React.FC = () => {
   return (
